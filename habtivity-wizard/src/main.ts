e3e837aaba2d049ff49e6ab34455801d556d10eb
Ankit <ankitkap@buffalo.edu>
2023-01-14 16:06:51 -0800

    Added vuetify. Works.

--- habtivity-wizard/src/main.ts
+++ habtivity-wizard/src/main.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import MainApp from './MainApp.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(MainApp)
 }).$mount('#app')
